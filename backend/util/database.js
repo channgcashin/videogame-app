38ca4d3bf9f029ca924352cddec0e28223c7b88d
channgcashin <71677568+channgcashin@users.noreply.github.com>
2024-05-17 11:46:30 -0400

    my sql changes

--- backend/util/database.js
+++ backend/util/database.js
@@ -1,6 +1,9 @@
 const mysql = require("mysql2")
 const config = require("../config/config.json")
 
+
+const urlDB = `mysql://root:oMAeltWarlWuagHUINuJioFDdPhrJZzf@monorail.proxy.rlwy.net:50228/railway`
+
 // Creates and connects db
 const pool = mysql.createPool({
     host: config.host,
